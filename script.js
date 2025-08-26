a9792fdf9c7afc32d054717bfacb2d12a64cd727
Shalindu Thushen <shalinduthushen@gmail.com>
2025-08-26 22:34:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.oneclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+// scroll sections
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+}
